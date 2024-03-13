ccf8fa052146817f9ac983f8431445c6be79f67a
jordanmontt <sebastianjmt@hotmail.com>
2024-03-13 22:51:31 +0100

    Changes to be committed: 	modified:   .gitignore 	new file:   python/df-analysis.ipynb 	deleted:    python/old-dataframe.ipynb 	new file:   python/overheads.ipynb 	new file:   python/plotting_new.py 	renamed:    python/new-notebook.ipynb -> python/previous-version-code/analysis-df-previous.ipynb 	renamed:    python/jupyter.ipynb -> python/previous-version-code/overhead-previous.ipynb 	renamed:    python/plotting.py -> python/previous-version-code/plotting.py 	renamed:    python/processor.py -> python/previous-version-code/processor.py 	new file:   python/processor_new.py

---
+++ python/plotting_new.py
@@ -0,0 +1,74 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import seaborn as sns
+import numpy as np
+
+# Overall frequencies
+
+def plot_relative_frequencies_histogram(title, df_100, df_50, df_1, df_01):
+    fig, axs = plt.subplots(ncols=4)
+    a_title = "Lifetime frequencies for " + title
+    fig.suptitle(a_title, fontsize=16)
+
+    sns.histplot(df_100['relativeLifetime'] , stat="percent", bins=20, ax=axs[0]).set(xlabel='Execution time', title='100% sampling', ylim=(0, 100))
+    sns.histplot(df_50['relativeLifetime'] , stat="percent", bins=20, ax=axs[1]).set(xlabel='Execution time', title='50% sampling', ylim=(0, 100))
+    sns.histplot(df_1['relativeLifetime'] , stat="percent", bins=20, ax=axs[2]).set(xlabel='Execution time', title='1% sampling', ylim=(0, 100))
+    sns.histplot(df_01['relativeLifetime'], stat="percent", bins=20, ax=axs[3]).set(xlabel='Execution time', title='0.1% sampling', ylim=(0, 100))
+
+    plt.tight_layout()
+    plt.show()
+
+def plot_overall_frequency(a_df, axs):
+    ax = sns.histplot(a_df['relativeLifetime'] , stat="percent", bins=20, ax=axs)
+    ax.set(xlabel='Execution time', ylim=(0, 100), xlim=(-5, 105))
+
+def plot_relative_frequencies_histogram_comparison(df_100, df_50, df_1, df_100_a, df_50_a, df_1_a):
+    fig, axs = plt.subplots(ncols=3, nrows=2)
+    a_title = "Overall lifetime frequencies comparison"
+    fig.suptitle(a_title, fontsize=16)
+
+    axs[0, 0].set_title("Sampling 100%")
+    axs[0, 1].set_title("Sampling 50%")
+    axs[0, 2].set_title("Sampling 1%")
+
+    plot_overall_frequency(df_100, axs[0, 0])
+    plot_overall_frequency(df_50, axs[0, 1])
+    plot_overall_frequency(df_1, axs[0, 2])
+    axs[0, 0].set_ylabel('Baseline profiler', fontsize=12)
+
+    plot_overall_frequency(df_100_a, axs[1, 0])
+    plot_overall_frequency(df_50_a, axs[1, 1])
+    plot_overall_frequency(df_1_a, axs[1, 2])
+    axs[1, 0].set_ylabel('Actionable profiler', fontsize=12)
+
+    plt.tight_layout()
+    plt.show()
+
+### By class
+
+def plot_lifetime_frequencies_by_class(top_allocated_classes, df_100, df_50, df_1, title, with_class_labels=True):
+    ncols = 3
+    fig, axs = plt.subplots(5, ncols, figsize=(5, 7))
+    fig.suptitle(title, fontsize=16)
+    axs[0, 0].set_title("Sampling 100%")
+    axs[0, 1].set_title("Sampling 50%")
+    axs[0, 2].set_title("Sampling 1%")
+
+    for idx, top_allocated_class in enumerate(top_allocated_classes):
+        plot_for_top_allocated_class(df_100, top_allocated_class, axs[idx, 0], (1 + ncols * idx))
+        plot_for_top_allocated_class(df_50, top_allocated_class, axs[idx, 1], (2 + ncols * idx))
+        plot_for_top_allocated_class(df_1, top_allocated_class, axs[idx, 2], (3 + ncols * idx))
+        if with_class_labels:
+            axs[idx, 0].set_ylabel(top_allocated_class, fontsize=12)
+
+    plt.tight_layout()
+    plt.show()
+
+def plot_for_top_allocated_class(a_df, top_allocated_class, an_axs, grid_position):
+    filtered_df = a_df.loc[ a_df['allocatedObjectClass'] == top_allocated_class ]
+
+    plt.subplot(5, 3, grid_position)
+    ax = sns.histplot(filtered_df['relativeLifetime'] , stat="percent", bins=3)
+    ax.set(ylabel="", xlabel="", xlim=(-5, 105), ylim=(0, 100), xticks=[0, 50, 100])
---
+++ python/processor_new.py
@@ -0,0 +1,85 @@
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+import tabulate
+
+
+def get_top_allocated_classes(a_dataframe):
+    # Returns a list with the classes that were allocated the most.
+    # To be a top allocator the class has to have at least 1% of the total allocations
+    counts = a_dataframe['allocatedObjectClass'].value_counts()
+    percs = a_dataframe['allocatedObjectClass'].value_counts(normalize=True)
+    # Get classes that were allocated and that represent at least 1% of the total allocations (calculating by number of allocations)
+    counts_and_percs = pd.concat([counts, percs], axis=1, keys=['count', 'percentage'])
+    top_allocated = counts_and_percs[ counts_and_percs['percentage'] >= 0.01 ]
+    #convert to a list
+    return top_allocated.axes[0].tolist()
+
+def filter_by_top_allocated(a_df):
+    top_allocated_classes = get_top_allocated_classes(a_df)
+    
+    dataframes = list()
+    for i, object_class in enumerate(top_allocated_classes):
+        dataframes.append(a_df[ a_df['allocatedObjectClass'] == object_class ])
+    return top_allocated_classes, dataframes
+
+# DATAFRAME
+
+def load_df(base_path):
+    csv_path = base_path + '.csv'
+    metadata_path = base_path + '.json'
+
+    a_df = pd.read_csv(csv_path)
+    f = open(metadata_path)
+    meta_data_of_df = json.load(f)
+    f.close()
+
+    meta_data_of_df['totalExecutionTime'] = meta_data_of_df['totalExecutionTime'] / 1000000 # to convert to seconds
+    # Add relative survived gc cycles
+    a_df['relativeSurvivedGCCycles'] = a_df['survivedFullGCs'].transform((lambda x : x / meta_data_of_df['totalFullGCs'] * 100 if x <= meta_data_of_df['totalFullGCs'] else 100) )
+    # Add relative survived scavenges
+    a_df['relativeSurvivedScavenges'] = a_df['survivedScavenges'].transform((lambda x : x / meta_data_of_df['totalScavenges'] * 100 if x <= meta_data_of_df['totalScavenges'] else 100) )
+    # Add relative lifetime in seconds
+    a_df['lifetime'] = (a_df['finalizationTimeInMicroSeconds'] - a_df['initializationTimeInMicroSeconds']) / 1000000 # to convert to seconds
+    a_df['relativeLifetime'] = a_df['lifetime'].transform((lambda x : x / meta_data_of_df['totalExecutionTime'] * 100 if x <= meta_data_of_df['totalExecutionTime'] else 100) )
+
+    return a_df, meta_data_of_df
+
+def print_table_allocations_average_lifetimes(df_title, a_df, metadata_df):
+    print(df_title.upper())
+    print()
+
+    top_allocated_classes, filtered_dfs = filter_by_top_allocated(a_df)
+    total_instances = len(a_df.index)
+    total_execution_time = metadata_df['totalExecutionTime']
+    total_full_gcs = metadata_df['totalFullGCs']
+    total_scavenges = metadata_df['totalScavenges']
+    total_allocated_memory = a_df['sizeInBytes'].sum()
+
+    print('Sampling rate:', str(metadata_df['sampligRate']))
+    print('Total instances: ', str(total_instances))
+    print('Total execution time:', str(total_execution_time))
+    print('Total full GCs: ', str(total_full_gcs))
+    print('Total scavenges: ', str(total_scavenges))
+    print('Total allocated memory: ', str(total_allocated_memory / 1024 / 1024), ' GiB')
+    print('----------')
+
+    my_list = []
+    headers = ['Allocated object class', 'Instances', 'Avg lifetime', 'Avg GC cycles', 'Avg Scavenges', 'Allocated memory']
+    for var_df in filtered_dfs:
+        relative_instances = (len(var_df.index) / total_instances) * 100
+        relative_full_gcs = var_df['relativeSurvivedGCCycles'].mean()
+        relative_lifetime = var_df['relativeLifetime'].mean()
+        relative_scavenges = var_df['relativeSurvivedScavenges'].mean()
+        relative_allocated_memory = (var_df['sizeInBytes'].sum() / total_allocated_memory) * 100
+
+        relative_instances = str(round(relative_instances, 2)) + '%'
+        relative_lifetime = str(round(relative_lifetime, 2)) + '%'
+        relative_full_gcs = str(round(relative_full_gcs, 2)) + '%'
+        relative_scavenges = str(round(relative_scavenges, 2)) + '%'
+        relative_allocated_memory = str(round(relative_allocated_memory, 2)) + '%'
+
+        my_list.append([var_df.iloc[0]['allocatedObjectClass'], relative_instances, relative_lifetime, relative_full_gcs, relative_scavenges, relative_allocated_memory])
+    
+    print(tabulate(my_list, headers=headers))
+    print()
